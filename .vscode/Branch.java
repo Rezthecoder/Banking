d828873d7062f3587e43d33d47c323e8e242a555
Suresh <suresh@gmail.com>
2022-08-13 23:02:21 +0900

    New class Added

--- .vscode/Branch.java
+++ .vscode/Branch.java
@@ -1,7 +1,42 @@
 import java.util.ArrayList;
 
 public class Branch {
-    ArrayList<Customer> customers;
-    
-    
+
+  private String name;
+  ArrayList<Customer> customers;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public boolean newCustomer(String name, double intialAmount) {
+    if (findCustomer(name) == null) {
+      this.customers.add(new Customer(name, intialAmount));
+      return true;
+    }
+    return false;
+  }
+
+  public boolean addCustomerTransation(String name, double amount) {
+    Customer existingCustomer = findCustomer(name);
+    if (existingCustomer != null) {
+      existingCustomer.addTransaction(amount);
+      return true;
+    }
+    return false;
+  }
+
+  private Customer findCustomer(String name) {
+    for (int i = 0; i < this.customers.size(); i++) {
+      Customer customer = this.customers.get(i);
+      if (customer.getName().equals(name)) {
+        return customer;
+      }
+    }
+    return null;
+  }
 }
--- .vscode/Customer.java
+++ .vscode/Customer.java
@@ -1,3 +1,30 @@
+import java.util.ArrayList;
+
 public class Customer {
-    
+
+  private String name;
+  ArrayList<Double> transactions;
+
+  public Customer(String name, double initialAmount) {
+    this.name = name;
+    this.transactions = new ArrayList<Double>();
+    addTransaction(initialAmount);
+  }
+
+  public void addTransaction(double amount) {
+    this.transactions.add(amount);
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+public ArrayList<Double> getTransactions() {
+    return transactions;
+}
+  
 }
